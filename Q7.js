ce514bb224e7ce6cec269fc35f14c31b1660773c
Mehreen676 <159542833+Mehreen676@users.noreply.github.com>
2024-02-23 21:15:08 +0500

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,21 @@
+"use strict";
+//(7) Number Eight: Write addition, subtraction, multiplication,
+//  and division operations that each result in the number 8.
+//  Be sure to enclose your operations in
+//  print statements to see the results.
+// (1)ADDITION
+let a = 5;
+let b = 3;
+console.log(a + b);
+// SUBSTRACTION
+let c = 13;
+let d = 5;
+console.log(c - d);
+// MULTIPLICATION
+let e = 2;
+let f = 4;
+console.log(e * f);
+// DIVISION
+let g = 32;
+let h = 4;
+console.log(g / h);
---
+++ Q7.ts
@@ -0,0 +1,24 @@
+//(7) Number Eight: Write addition, subtraction, multiplication,
+//  and division operations that each result in the number 8.
+//  Be sure to enclose your operations in
+//  print statements to see the results.
+
+// (1)ADDITION
+let a:number =5
+let b:number =3
+console.log(a+b)
+
+// SUBSTRACTION
+let c:number =13
+let d:number =5
+console.log(c-d)
+
+// MULTIPLICATION
+let e:number =2
+let f:number =4
+console.log(e * f)
+
+// DIVISION
+let g:number =32
+let h:number =4
+console.log(g / h)
